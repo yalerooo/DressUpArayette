9ab0d6569e3bd78102d3d8924786fda7152096c3
yalerooo <yaleros2@gmail.com>
2025-02-11 16:52:38 +0100

    vercel prueba

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js (Si NO tienes un basePath, mejor, déjalo así)
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
