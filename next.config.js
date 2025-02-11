f6f498d2a55484d87cbdd80fde6b4b53e932841b
yalerooo <yaleros2@gmail.com>
2025-02-11 16:26:35 +0100

    update to vercel2

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js (JavaScript - SÍ funcionará)
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Otras configuraciones...
+};
+
+module.exports = nextConfig;
